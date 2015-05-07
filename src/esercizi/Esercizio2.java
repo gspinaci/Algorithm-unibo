3ff529fcfb09b2554dee810eae2c9f1cb513658b
Gianmarco <spino9330@gmail.com>
2015-05-07 21:11:00 +0200

    Completamento Esercizio2
    Completamente del secondo esercizio, inizio a guardare il quarto

--- src/esercizi/Esercizio2.java
+++ src/esercizi/Esercizio2.java
@@ -1,14 +1,38 @@
 /**
  * 
  * 
- * Gianmarco Spinaci
+ * Gianmarco 
+ * Spinaci
  * 0000691241
  * gianmarco.spinaci@studio.unibo.it
  * 
+ * REPORT:
  * 
- * 6/05/15
+ * DATA -> 6/05/15
  * 
  * funziona solo con il primo input, non ha conoscenza del futuro. non può fare scelte ottimali, approccio greedy
+ * 
+ * ----------------------------
+ * 
+ * DATA -> 7/05/15
+ * 
+ * trovato algoritmo per approccio dinamico
+ * 
+ * 
+ * il primo metodo che ho trovato era un metodo poco efficiente di O(n*m^2)
+ * 
+ * valutava per ogni obbligazione( [t][j] ) il valore massimo tra i valori dati dalle combinazioni 
+ * tra tutte obbligazioni t-1 
+ * 
+ * il metodo efficiente che ho pensato è invece O(n*m)
+ * 
+ * non è necessario andare a valutare tutte le combinazioni dell'obbligazione( [t][j]) 
+ * con le obbligazioni del mese t-1, ma basta valutarla solo con il valore massimo del mese t-1
+ * nel nostro caso c'era la possibilità di avere una penale se da un mese all'altro si cambiava obbligazione
+ *
+ * basta gestire il caso grazie al massimo tra i due parametri
+ * 
+ * CONCLUSO
  * 
  */
 
@@ -28,16 +52,20 @@ public class Esercizio2 {
 		int numeroMesi, numeroObbligazioni;
 		double penale;
 		
+		//creazione di 3 matrici
+		//una contiene i valori delle obbligazioni dati da input 
+		//una che uso per la programmazione dinamica
+		//l'ultima che uso per tener traccia del percorso di scelta che faccio nel tempo
 		double R[][];
 		double S[][];
+		int X[][];
 		
 
 		//prevenzione FileNotFoundException
 		try{
 			
-			//buffered* utilizzate per lettura e scrittura file
+			//buffered* utilizzate per lettura file
 			reader = new BufferedReader(new FileReader(new File("src/Esercizio2-3.in")));
-			//writer = new BufferedWriter(new FileWriter(new File("src/Esercizio2-1.out")));
 			
 			//lettura file e settaggio variabili
 			penale = Double.parseDouble(reader.readLine());
@@ -47,8 +75,9 @@ public class Esercizio2 {
 			//inizializzazione matrice contenente le obbligazioni divise per mese
 			R = new double[numeroMesi][numeroObbligazioni];
 			S = new double[numeroMesi][numeroObbligazioni];
+			X = new int[numeroMesi][numeroObbligazioni];
 			
-			//popolamento matrice
+			//popolamento matrici
 			for(int i = 0;i<numeroMesi;i++){
 				
 				String[] row = reader.readLine().split("\\s+");
@@ -57,14 +86,29 @@ public class Esercizio2 {
 					
 					R[i][j] = Double.parseDouble(row[j]);
 					S[i][j] = 0;
+					X[i][j] = 0;
 				}
 			}
 			
 			
 			//creazione dell'oggetto che serve per semplificare la comprensione del codice
-			Finanziaria fin = new Finanziaria(R,S,numeroObbligazioni,numeroMesi,penale);
+			Finanziaria fin = new Finanziaria(R,S,X,numeroObbligazioni,numeroMesi,penale);
 			
-			System.out.println(fin.getMassimoValore());
+			//salvo la somma del valore massimo che posso avere con queste obbligazioni in input
+			//il metodo richiamato è effettivamente il codice dell'algoritmo dell'esercizio
+			double somma = fin.getMassimoValore();
+			
+			//trovo il percorso che mi porta ad avere il valore massimo
+			int[] tmp = fin.getPath();
+			
+			
+			//scrittura output
+			for(int x : tmp){
+				
+				System.out.println(x);
+			}
+			
+			System.out.println(somma);
 			
 		}
 		
@@ -75,24 +119,41 @@ public class Esercizio2 {
 	}
 }
 
+/**
+ * 
+ * @author Gianmarco
+ *
+ */
+
 class Finanziaria{
 	
 	double[][] R;
 	double[][] S;
+	int[][] X;
 	int n,m;
 	double p;
 	
 	
-	public Finanziaria(double[][] R, double[][] S, int n, int m, double p){
+	//costruttore per inizializzare
+	public Finanziaria(double[][] R, double[][] S,int[][] X, int n, int m, double p){
 		
 		this.R = R;
+		this.S = S;
+		this.X = X;
+		
 		this.n = n;
 		this.m = m;
 		this.p = p;
-		this.S = S;
 	}
-	
-	public int getMaxValueIndex(double[] row){
+	/**
+	 * 
+	 * metodo che accetta un vettore di N elementi double in input
+	 * e ritorna l'indice dell'elemento maggiore
+	 * 
+	 * O(n)
+	 * 
+	 */
+	public int getMaxValueIndex(double... row){
 		
 		double max = row[0];
 		int index = 0;
@@ -109,6 +170,14 @@ class Finanziaria{
 		return index;
 	}
 
+	/**
+	 * 
+	 * metodo che accetta un vettore di N elementi double in input
+	 * e ritorna il valore double maggiore
+	 * 
+	 * O(n)
+	 * 
+	 */
 	public double getMaxValue(double ... row){
 		
 		double max = row[0];
@@ -125,15 +194,30 @@ class Finanziaria{
 		
 	}
 		
+	
+	/**
+	 * 
+	 * algoritmo principale 
+	 * 
+	 * sfruttando la programmazione dinamica sono riuscito ad utilizzare una matrice dei risultati
+	 * che vado a popolare dinamicamente
+	 * ad ogni mese (tranne il primo) valuta la combinazione migliore di obbligazioni
+	 * 
+	 * versioni algoritmo [vedi report]
+	 * 
+	 * versione corrente :
+	 * 
+	 * costo O(n*m)
+	 * 
+	 */
 	public double getMassimoValore(){
 		
 		//inizializzo la prima riga
 		for(int i=0; i<n; i++){
 			
 			S[0][i] = R[0][i];
-			
+			X[0][i] = -1;
 		}
-		
 		
 		//setto l'indice dell'elemento di valore massimo, del mese t=0
 		int currentIndex = getMaxValueIndex(S[0]);
@@ -156,6 +240,14 @@ class Finanziaria{
 				S[t][j] = R[t][j] + getMaxValue( S[t-1][currentIndex]-p, S[t-1][j] );
 				
 				
+				//popolo la matrice che mi serve per tenere conto del percorso 
+				if ( S[t-1][currentIndex]-p > S[t-1][j] )
+					
+					X[t][j] = currentIndex;
+				
+				else
+					
+					X[t][j] = j;
 			}
 			
 			//salvo l'indice dell'elemento maggiore del mese corrente
@@ -166,5 +258,25 @@ class Finanziaria{
 		//cioè il massimo valore che si può ottenere con il giusto utilizzo delle obbligazioni
 		return getMaxValue(S[S.length-1]);
 	}
-	
+
+	/**
+	 * 
+	 * metodo che ricostruisce il percorso ideale nello scegliere le obbligazioni,
+	 * leggendo la matrice al contario
+	 * 
+	 */
+	public int[] getPath(){
+		
+		int[] ris = new int[m];
+		
+		ris[m-1]  = getMaxValueIndex(S[S.length-1]);
+		
+		for(int t=m-2; t>=0; t--){
+			
+			ris[t] =  X[t+1][ris[t+1]];
+		}
+		
+		return ris;
+	}
+
 }
