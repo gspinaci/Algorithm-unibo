d579e0dadac091dd00e225568309958af0a45657
Gianmarco <spino9330@gmail.com>
2015-05-08 11:41:27 +0200

    Inizializione Esercizio4
    ho iniziato a scrivere il procedimento del quarto esercizio

---
+++ src/esercizi/Esercizio4.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * 
+ * Gianmarco 
+ * Spinaci
+ * 0000691241
+ * gianmarco.spinaci@studio.unibo.it
+ * 
+ * REPORT:
+ * 
+ * DATA -> 8/05/15
+ * 
+ * ho trovato un algoritmo di risoluzione che va con O(n^2)
+ * 
+ * creo una matrice NxN, che contiene le distanze tra tutte le città
+ * essendo una matrice speculare mi basta lavorare su (N^2 - N)/2 elementi della matrice
+ * 
+ * creazione della matrice Θ( (N^2 - N)/2 )
+ * 
+ * ad ogni riga
+ * 
+ * 
+ * 
+ */
+
+package esercizi;
+
+public class Esercizio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
